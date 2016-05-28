a02b2a228c833f344a9b95ce41a82fdb9fc20b2f
ELJei03 <eljei.delrio@bmwaresd.com>
2016-05-27 17:48:57 -0700

    1.modal_icons_update

--- application/views/templates/top_navigation.php
+++ application/views/templates/top_navigation.php
@@ -13,46 +13,7 @@
 										<span class="m-r-sm text-muted welcome-message">Welcome
 										<strong class="text-info "><?php echo  $this->session->userdata('fullname'); ?> </strong> Have a nice day.</span>
 									</li>
-									<li class="dropdown">
-										<a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
-											<i class="fa fa-envelope"></i>  <span class="label label-warning">2</span>
-										</a>
-										<ul class="dropdown-menu dropdown-messages">
-											<li>
-												<div class="dropdown-messages-box">
-													<a href="profile.html" class="pull-left">
-														<img alt="image" class="img-circle" src="assets/img/a7.jpg">
-													</a>
-													<div class="media-body">
-														<small class="pull-right">46h ago</small>
-														<strong>JDEV IT Business Solutions</strong> sent you a message. <br>
-														<small class="text-muted">3 days ago at 7:58 pm - 10.06.2014</small>
-													</div>
-												</div>
-											</li>
-											<li class="divider"></li>
-											<li>
-												<div class="dropdown-messages-box">
-													<a href="profile.html" class="pull-left">
-														<img alt="image" class="img-circle" src="assets/img/a4.jpg">
-													</a>
-													<div class="media-body ">
-														<strong>Mr. Rueda</strong> sent you a message. <br>
-														<small class="text-muted">3 days ago at 7:58 pm - 10.06.2014</small>
-													</div>
-												</div>
-											</li>
-											
-											<li class="divider"></li>
-											<li>
-												<div class="text-center link-block">
-													<a href="#">
-														<i class="fa fa-envelope"></i> <strong>Read All Messages</strong>
-													</a>
-												</div>
-											</li>
-										</ul>
-									</li>
+								
 								
 
 
